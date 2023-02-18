f3ad173620cd5c8a0d849eed1eccbbe6460ca377
surydeepak <surydeepak@gmail.com>
2023-02-19 00:56:30 +0530

    Shopping controller

---
+++ ShoppingCart/APIGateway/Bff.Shopping/Shopping.Aggregator/Controller/ShoppingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping.Aggregator.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+
+
+
+
+    }
+}
